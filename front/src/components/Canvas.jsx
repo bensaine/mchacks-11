a7bc7327faf54445c4b37a19889f8dc228cb70e5
Vincent <vincentvibe3@gmail.com>
2024-01-27 18:52:16 -0500

    Fix overflow on canvas

--- front/src/components/Canvas.jsx
+++ front/src/components/Canvas.jsx
@@ -1,106 +1,170 @@
-import { ReactP5Wrapper } from "@p5-wrapper/react"
-import { socket } from "../socket"
-import { nanoid } from "nanoid"
+import { ReactP5Wrapper } from "@p5-wrapper/react";
+import { socket } from "../socket";
+import { nanoid } from "nanoid";
+import { css } from "@emotion/react";
+
+const boothGap = 60;
+const boothColumns = 3;
 
 const boothsData = [
-	{ name: "SSMU", image: "/ssmu.svg", x:100,  y:100 },
-	{ name: "SSMU", image: "/ssmu.svg", x:-100,  y:100 },
-	{ name: "SSMU", image: "/ssmu.svg", x:100,  y:-100 },
-	{ name: "SSMU", image: "/ssmu.svg", x:-100,  y:-100 }
-]
+	{ name: "SSMU", image: "/ssmu.svg" },
+	{ name: "Ensemble", image: "/Ensemble.png" },
+	{ name: "Bell", image: "/Bell.png" },
+	{ name: "NordVPN", image: "/Nord.png" },
+	{ name: "Google Cloud", image: "/GoogleCloud.png" },
+	{ name: "Telus", image: "/Telus.png" },
+	{ name: "SpiceBros", image: "/SpiceBros.png" },
+	{ name: "Incogni", image: "/incogni.png" },
+];
 
-const boothSize = 90
+const boothSize = 90;
+const boothPadding = 50;
+const textSize = 12;
 
 function sketch(p5) {
-	let boothObjs
-	let font
+	let boothObjs;
+	let boothLocations;
+	let font;
+	let emojiFont;
+	let floor;
+	let boothLayer;
 
-	window.p5 = p5
+	window.p5 = p5;
 	class World {
 		constructor() {
-			this.player = new Player()
-			this.player.init()
-			this.otherPlayers = {}
+			this.player = new Player();
+			this.player.init();
+			this.otherPlayers = {};
 		}
 		updateState(state) {
-			this.otherPlayers = state
+			this.otherPlayers = state;
 		}
 		handleKeys(code) {
 			if (code in keysMapping) {
-				const displacement = keysMapping[code]
-				console.log(`displacememt is ${displacement.x} ${displacement.y}`)
-				this.player.updatePosition(displacement)
+				const displacement = keysMapping[code];
+				console.log(`displacememt is ${displacement.x} ${displacement.y}`);
+				this.player.updatePosition(displacement);
 			}
 		}
 		display() {
-			this.player.display()
+			this.player.display();
 			for (const [key, { x, y }] of Object.entries(this.otherPlayers)) {
-				const tempPlayer = new Player()
-				tempPlayer.setPosition(x, y)
-				tempPlayer.display()
+				const tempPlayer = new Player();
+				tempPlayer.setPosition(x, y);
+				tempPlayer.display();
 			}
 		}
 	}
 	// debugger
 	class Player {
 		constructor() {
-			this.position = p5.createVector(0, 0)
+			this.position = p5.createVector(0, 0);
 		}
 		display() {
-			p5.fill(255, 255, 255)
-			p5.ellipse(this.position.x, this.position.y, 30, 30)
+			p5.fill(255, 255, 255);
+			p5.ellipse(this.position.x, this.position.y, 30, 30);
+			p5.fill("black");
+			p5.textFont(emojiFont);
+			p5.textSize(textSize);
+			p5.textAlign(p5.CENTER);
+			p5.text("👨", this.position.x, this.position.y+textSize/2)
 		}
 
 		init() {
-			this.id = nanoid()
-			alert("putting nanoid once!")
-			socket.connect()
+			this.id = nanoid();
+			// alert("putting nanoid once!");
+			socket.connect();
 		}
 
 		setPosition(x, y) {
-			this.position = p5.createVector(x, y)
+			this.position = p5.createVector(x, y);
 		}
 
 		updatePosition(displacement) {
-			this.position.add(displacement)
+			this.position.add(displacement);
 			// console.log(`updated position to ${this.position.x} ${this.position.y}`)
-			const { x, y } = this.position
-			const id = this.id
-			socket.emit("user:update", { x, y, id })
+			const { x, y } = this.position;
+			const id = this.id;
+			socket.emit("user:update", { x, y, id });
 		}
 	}
-	const world = new World()
+	const world = new World();
 	socket.on("state:update", (state) => {
-		world.updateState(state)
-	})
+		world.updateState(state);
+	});
 
 	function loadBooths() {
-		let booths = []
+		let booths = [];
+		boothLocations = []
+		let rowCount = Math.ceil(boothsData.length / boothColumns);
+		let yCenter = Math.floor(rowCount / 2)
+		let xCenter = Math.floor(boothColumns / 2)
+		let currentRow = 0;
+		let rowPos = 0;
+		let xOffset = (boothColumns * (boothSize + boothGap)) / 2;
+		let yOffset = (rowCount * (boothSize + boothPadding + boothGap)) / 2;
 		for (let booth of boothsData) {
-			booths.push({ imgObj: p5.loadImage(booth.image), name: booth.name, x:booth.x, y:booth.y })
+			booths.push({
+				imgObj: p5.loadImage(booth.image),
+				name: booth.name,
+			});
+			boothLocations[booth.name] = {
+				x: (boothSize + boothPadding + boothGap) * rowPos - xOffset,
+				y:
+					currentRow * (boothSize + boothPadding + textSize * 2 + boothGap) -
+					yOffset,
+			}
+			if (currentRow === yCenter && xCenter == rowPos + 1) {
+				rowPos++
+			}
+			if (rowPos === boothColumns - 1) {
+				currentRow++;
+				rowPos = 0;
+			} else {
+				rowPos++;
+			}
 		}
-		return booths
+		return booths;
 	}
 
-	function drawBooths(){
-		for (let booth of boothObjs){
-			drawBooth(booth.imgObj, booth.name, booth.x, booth.y, boothSize)
+	function drawBooths() {
+		for (let booth of boothObjs) {
+			drawBooth(booth.imgObj, booth.name, boothLocations[booth.name].x, boothLocations[booth.name].y, boothSize);
 		}
+		boothObjs = null
 	}
 
 	function drawBooth(imageObj, name, x, y, size) {
-		let textSize = 12
-		p5.textFont(font)
-		p5.textSize(textSize)
-		p5.textAlign(p5.CENTER)
-		p5.text(name, x, y)
-		p5.image(imageObj, x - size / 2, y + textSize, size, size)
+		boothLayer.textFont(font);
+		boothLayer.textSize(textSize);
+		boothLayer.textAlign(p5.CENTER);
+		boothLayer.fill("black");
+		boothLayer.text(name, x, y);
+		let largestMeasurement = Math.max(imageObj.width, imageObj.height);
+		let ratio = 1;
+		if (largestMeasurement > size) {
+			ratio = size / largestMeasurement;
+		}
+		let width = imageObj.width * ratio;
+		let height = imageObj.height * ratio;
+		boothLayer.strokeWeight(2);
+		boothLayer.stroke("gray");
+		boothLayer.fill(0, 0, 0, 0);
+		boothLayer.rect(
+			x - width / 2 - boothPadding / 2,
+			y - textSize - boothPadding / 2,
+			width + boothPadding,
+			height + textSize * 2 + boothPadding,
+			2
+		);
+		boothLayer.image(imageObj, x - width / 2, y + textSize, width, height);
+		boothLayer.noStroke();
 	}
 
-	const UP = p5.createVector(0, -1)
-	const DOWN = p5.createVector(0, 1)
-	const LEFT = p5.createVector(-1, 0)
-	const RIGHT = p5.createVector(1, 0)
+	const UP = p5.createVector(0, -1);
+	const DOWN = p5.createVector(0, 1);
+	const LEFT = p5.createVector(-1, 0);
+	const RIGHT = p5.createVector(1, 0);
 	const keysMapping = {
 		ArrowUp: UP,
 		KeyW: UP,
@@ -110,32 +174,59 @@ function sketch(p5) {
 		KeyA: LEFT,
 		ArrowRight: RIGHT,
 		KeyD: RIGHT,
-	}
+	};
+
 	function addKeyListeners() {
 		document.addEventListener("keydown", (e) => {
-			world.handleKeys(e.code)
-		})
+			world.handleKeys(e.code);
+		});
 	}
 
 	p5.preload = () => {
-		boothObjs = loadBooths()
-		font = p5.loadFont("/fonts/NotoSans-Regular.ttf")
-	}
+		boothObjs = loadBooths();
+		floor = p5.loadImage("/floor.jpg");
+		font = p5.loadFont("/fonts/NotoSans-Regular.ttf");
+		emojiFont = p5.loadFont("/fonts/NotoColorEmoji-Regular.ttf");
+	};
 
 	p5.setup = () => {
-		p5.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL)
-		addKeyListeners()
-	}
+		p5.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL);
+		boothLayer = p5.createGraphics(window.innerWidth, window.innerHeight, p5.WEBGL)
+		drawBooths()
+		addKeyListeners();
+	};
 
 	p5.draw = () => {
-		p5.background(0, 0, 0)
-		world.display()
-		drawBooths()
-	}
+		p5.background(0, 0, 0);
+		p5.image(
+			floor,
+			-p5.displayWidth / 2,
+			-p5.displayHeight / 2,
+			p5.displayWidth,
+			p5.displayHeight
+		);
+		p5.image(boothLayer, -p5.displayWidth / 2,
+			-p5.displayHeight / 2,
+			p5.displayWidth,
+			p5.displayHeight)
+		world.display();
+	};
 }
+
+let divStyle = css`
+	display:flex;
+	flex-direction:row;
+	align-items:center;
+	justify-content:center;
+	height:100vh;
+	width:100vw;
+	overflow:hidden;
+`
 
 const Canvas = () => {
-	return <ReactP5Wrapper sketch={sketch} />
-}
+	return <div css={divStyle}>
+		<ReactP5Wrapper sketch={sketch} />
+	</div>;
+};
 
-export default Canvas
+export default Canvas;
