151be63b016dce5070fd9f6dcc1687d8301921d1
James Liang <james.liang@umontreal.ca>
2024-01-28 11:53:31 -0500

    done!

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
 
 const URL = window.location.hostname === "careerfairy.club" ? "https://45.77.144.218" : "http://127.0.0.1:4000"
-export const socket = io('http://127.0.0.1:4000')
+export const socket = io(URL)
 export default socket
